a26bf54a88cfd666669add3b8a19786817089deb
AdedolapoAkanle <dolapoakanle@gmail.com>
2022-10-11 11:42:08 +0100

    new commit

---
+++ connection.php
@@ -0,0 +1,8 @@
+
+<?php
+    $connection = mysqli_connect("localhost", "root", "", "register");
+
+    if(!$connection) {
+        // die("Database connection failed" . mysqli_error());
+    }
+?>
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+    require("connection.php");
     header("Access-Control-Allow-Origin: *");
     header("Content-Type: application/json; charset=UTF-8");
     header("Access-Control-Allow-Methods: POST");
@@ -8,17 +8,28 @@
     
     $json = file_get_contents('php://input');
     $body = json_decode($json, true);
+    
+    $firstName = $body['firstName'];
+    $lastName = $body['lastName'];
+    $address = $body['address'];
+    $age = $body['age'];
+    $gender = $body['gender'];
 
-    // $firstName = $body['firstName'];
-    // $lastName = $body['lastName'];
-    // $age = $body['age'];
+    // $firstName = $_REQUEST['firstName'];
+    // $lastName = $_REQUEST['lastName'];
+    // $address = $_REQUEST['address'];
+    // $age = $_REQUEST['age'];
+    // $gender = $_REQUEST['gender'];
+    
+    if(empty($firstName) || empty($lastName) || empty($address) || empty($age) || empty($gender)) {
+        echo json_encode("None of the fields must be empty");
+        exit;
+    }
 
-    // if(empty($firstName)) {
-    //     echo json_encode("First name cannot be empty");
-    //     exit;
-    // }
-
+    $sql = "INSERT INTO user SET first_name= '$firstName',last_name= '$lastName',address= '$address',age= '$age',gender= '$gender'";
+    mysqli_query($connection, $sql);
+    
     // save into db
-    echo json_encode("Response");
+    echo json_encode("Registration Successful");
     exit;
 ?>
